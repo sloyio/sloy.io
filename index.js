57ba37b86130aa05e0157156b0c8f377bea73ac8
Anton Akhatov <anton@gcor.ru>
2024-07-27 15:58:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const copyToClipboard = (str) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+    return navigator.clipboard.writeText(str);
+  return Promise.reject("The Clipboard API is not available.");
+};
+
+document.querySelector("#js_copy_link").addEventListener("click", function (e) {
+  copyToClipboard("sloy.io");
+  e.target.textContent = "✓";
+});
